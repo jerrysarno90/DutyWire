349dbc36802b8c56282d4739d58e54afb79ce477
Gerald Sarno <Jerry.Sarno90@gmail.com>
2025-11-02 14:17:07 -0500

    Initial Commit

---
+++ ShiftlinkMain/ShiftlinkMainApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftlinkMainApp.swift
+//  ShiftlinkMain
+//
+//  Created by Jerry Sarno on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ShiftlinkMainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShiftlinkMainTests/ShiftlinkMainTests.swift
@@ -0,0 +1,16 @@
+//
+//  ShiftlinkMainTests.swift
+//  ShiftlinkMainTests
+//
+//  Created by Jerry Sarno on 11/2/25.
+//
+
+import Testing
+
+struct ShiftlinkMainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
